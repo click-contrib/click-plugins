ace929f67084bdf9d883196381638e71dc658a2b
Kevin Wurster <wursterk@gmail.com>
2017-01-14 00:15:15 -0500

    Better broken plugin exception formatting and general cleanup.

--- click_plugins/core.py
+++ click_plugins/core.py
@@ -1,27 +1,23 @@
-"""
-Core components for click_plugins
-"""
+"""Core components for click_plugins"""
 
-
-import click
 
 import os
 import sys
 import traceback
 
+import click
+
 
 def with_plugins(plugins):
 
-    """
-    A decorator to register external CLI commands to an instance of
-    `click.Group()`.
+    """A decorator to register external CLI commands to an instance of
+    ``click.Group()``.
 
     Parameters
     ----------
-    plugins : iter
-        An iterable producing one `pkg_resources.EntryPoint()` per iteration.
-    attrs : **kwargs, optional
-        Additional keyword arguments for instantiating `click.Group()`.
+    entry_points : iter
+        An iterable producing one ``pkg_resources.EntryPoint()`` per
+        iteration.
 
     Returns
     -------
@@ -30,16 +26,18 @@ def with_plugins(plugins):
 
     def decorator(group):
         if not isinstance(group, click.Group):
-            raise TypeError("Plugins can only be attached to an instance of click.Group()")
+            raise TypeError(
+                "Plugins can only be attached to an instance of "
+                "'click.Group()'.")
 
-        for entry_point in plugins or ():
+        for ep in plugins:
             try:
-                group.add_command(entry_point.load())
+                group.add_command(ep.load())
             except Exception:
                 # Catch this so a busted plugin doesn't take down the CLI.
                 # Handled by registering a dummy command that does nothing
                 # other than explain the error.
-                group.add_command(BrokenCommand(entry_point.name))
+                group.add_command(BrokenCommand(ep.name))
 
         return group
 
@@ -48,41 +46,53 @@ def with_plugins(plugins):
 
 class BrokenCommand(click.Command):
 
-    """
-    Rather than completely crash the CLI when a broken plugin is loaded, this
-    class provides a modified help message informing the user that the plugin is
-    broken and they should contact the owner.  If the user executes the plugin
-    or specifies `--help` a traceback is reported showing the exception the
-    plugin loader encountered.
+    """Rather than completely crash the CLI when a broken plugin is
+    loaded, this class provides a modified help message informing the
+    user that the plugin is broken and they should contact the owner.
+    If the user executes the plugin or specifies ``--help`` a traceback
+    is reported showing the exception the plugin loader encountered.
     """
 
     def __init__(self, name):
 
-        """
-        Define the special help messages after instantiating a `click.Command()`.
+        """Define the special help messages after instantiating a
+        ``click.Command()``.
+
+        Parameters
+        ----------
+        name : str
+            For ``click.Command()``.
         """
 
         click.Command.__init__(self, name)
-
-        util_name = os.path.basename(sys.argv and sys.argv[0] or __file__)
 
         if os.environ.get('CLICK_PLUGINS_HONESTLY'):  # pragma no cover
             icon = u'\U0001F4A9'
         else:
             icon = u'\u2020'
 
+        # Override the command's short help with a warning message about how
+        # the command is not functioning properly
+        prog_name = os.path.basename(sys.argv and sys.argv[0] or __file__)
+        self.short_help = (
+            "{icon} Warning: could not load plugin.  See: "
+            "'$ {prog_name} {name} --help'.".format(
+                icon=icon, prog_name=prog_name, name=name))
+
+        # Override the command's long help with the exception traceback
         self.help = (
             "\nWarning: entry point could not be loaded. Contact "
-            "its author for help.\n\n\b\n"
-            + traceback.format_exc())
-        self.short_help = (
-            icon + " Warning: could not load plugin. See `%s %s --help`."
-            % (util_name, self.name))
+            "its author for help.\n\n\b\n".format(os.linesep)
+            + traceback.format_exc(chain=False))
 
     def invoke(self, ctx):
 
-        """
-        Print the traceback instead of doing nothing.
+        """Print the traceback instead of doing nothing.
+
+        Parameters
+        ----------
+        ctx : click.Context
+            CLI context.
         """
 
         click.echo(self.help, color=ctx.color)
